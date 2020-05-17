d101e4f6306b2278640e4c83dfe2b92f6d3dd36a
Дмитрий <tomountain@mail.ru>
2020-05-17 23:39:20 +0400

    feat: one solution

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -106,28 +106,13 @@ function getFastestPromise(array) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  // throw new Error('Not implemented');
 
-  // const result = array.reduce(action);
-  // return Promise.resolve(result).then((value) => console.log(value));
-  /*
-  const promise = array.reduce((a) => a.then(() => array.reduce(action)), Promise.resolve());
-  return promise.then((value) => console.log(value));
-*/
-  // const promise = array.reduce((prev) => prev
-  //   .then(), Promise.resolve());
-  // return promise.then((value) => value.reduce(action));
-
-  // const promise = array.reduce((prev) => prev
-  //   .then(() => array.reduce(action)), Promise.resolve());
-  // return promise.then((value) => console.log(value));
-
-/*
-  const promise = array.reduce((prev, cur) => prev
-    .then(value => value + cur), Promise.resolve());
-  return promise.then((value) => value);
-*/
+  // const promise = array.reduce(action, Promise.resolve());
+  const promise = array.map((value) => Promise.resolve(value)).reduce(action);
+  // const promise = chainPromises(promises, action);
+  return promise.then((data) => data);
 }
 
 module.exports = {
--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -138,8 +138,9 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  console.log(func);
+  console.log(logFunc);
 }
 
 
@@ -178,8 +179,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let value = startFrom - 1;
+  return function () {
+    value += 1;
+    return value;
+  };
 }
 
 
